a581346b672720e469974478b7e5938ed9ca53a3
jretac <jorge.reta.carcamo@gmail.com>
2023-09-06 17:14:55 +0200

    Initial version

---
+++ devices.py
@@ -0,0 +1,147 @@
+import socket
+
+import paho.mqtt.client as mqtt
+import logging
+import time
+
+
+
+class PlugDevice:
+    def __init__(self, mqtt_user: str,
+                 mqtt_password: str,
+                 mqtt_broker: str,
+                 mqtt_device_id: str,
+                 mqtt_port: int = 1883,
+                 mqtt_keepalive: int = 60):
+        """
+        Creates a plug device with an MQTT client to control a Shelly 1 Plug
+        :param mqtt_user:
+        :param mqtt_password:
+        :param mqtt_broker: broker IP address
+        :param mqtt_device_id: Shelly 1 Plug Id
+        :param mqtt_port: default 1883
+        """
+        self._user = mqtt_user
+        self._pwd = mqtt_password
+        self._broker_url = mqtt_broker
+        self._port = mqtt_port
+        self._device_id = mqtt_device_id
+        self._keepalive = mqtt_keepalive
+        self.temperature = None
+        self.state = None
+        self.power = None
+        self._log_interval = 300
+        self._subscription_tstamps = {}
+        self._last_connect_rc = None
+        self.mqtt_client = mqtt.Client()
+        self.mqtt_client.on_connect = self._on_connect
+        self.mqtt_client.on_message = self._on_message
+        self._logger = logging.getLogger(__name__)
+
+    def connect(self) -> int:
+        """
+        Connects the plug device to the MQTT broker. In case of error, description is logged to main logger.
+        It only needs to be called once
+        :return: MQTT error code, check devices.mqtt.MQTT_ERR_... for more details
+        """
+        if self.is_connected():
+            return mqtt.MQTT_ERR_SUCCESS
+        self._logger.info(f'Connecting to mqtt broker: {self._broker_url} with user: {self._user}')
+        self.mqtt_client.username_pw_set(self._user, self._pwd)
+        try:
+            self.mqtt_client.connect(self._broker_url, self._port, self._keepalive)
+        except socket.timeout as excpt:
+            self._logger.error(f'Could not connect to broker: {excpt.args[0]}')
+            return mqtt.MQTT_ERR_NO_CONN
+        except ConnectionRefusedError as excpt:
+            self._logger.error(f'Connection refused from broker. Reason: {excpt.args[0]}')
+            return mqtt.MQTT_ERR_CONN_REFUSED
+
+        self.mqtt_client.loop_start()
+        return mqtt.MQTT_ERR_SUCCESS
+
+    def disconnect(self):
+        self._logger.info(f'Disconnecting from mqtt broker: {self._broker_url} with user: {self._user}')
+        self.mqtt_client.disconnect()
+
+    def is_connected(self):
+        return self.mqtt_client.is_connected()
+
+    def device_on(self):
+        self.connect()
+        if self.state == 'off' or self.state is None:
+            self._logger.info('Sending Device ON')
+            self.mqtt_client.publish(f'shellies/{self._device_id}/relay/0/command', 'on')
+
+    def device_off(self):
+        self.connect()
+        if self.state == 'on' or self.state is None:
+            self._logger.info('Sending Device OFF')
+            self.mqtt_client.publish(f'shellies/{self._device_id}/relay/0/command', 'off')
+
+    def device_toggle(self):
+        self.connect()
+        self._logger.info('Sending Device TOGGLE')
+        self.mqtt_client.publish(f'shellies/{self._device_id}/relay/0/command', 'toggle')
+
+    def subscribe_to_device(self):
+        self.connect()
+        topic_list = [(f'shellies/{self._device_id}/temperature', 0),
+                      (f'shellies/{self._device_id}/relay/0/power', 0),
+                      (f'shellies/{self._device_id}/relay/0', 0),
+                      (f'plug/data', 0),
+                      (f'plug/data/info', 0)]
+        (result, mid) = self.mqtt_client.subscribe(topic_list)
+        if result == mqtt.MQTT_ERR_SUCCESS:
+            for topic in topic_list:
+                self._subscription_tstamps[topic[0]] = time.time()
+            self._logger.info(f'Subscribed to topics successful')
+        else:
+            self._logger.error(f'Subscription failed: MQTT client not connected')
+
+    def _on_connect(self, client, userdata, flags, rc):
+        if rc == 5:
+            # mqtt library returns rc = 5 even when credentials are incorrect
+            self._logger.error(f'Connection result: {mqtt.connack_string(4)}')
+            self._last_connect_rc = 4
+        else:
+            self._logger.info(f'Connection result: {mqtt.connack_string(rc)}')
+        self._last_connect_rc = rc
+
+    def _on_message(self, client, userdata, message: mqtt.MQTTMessage):
+        if 'temperature' == message.topic.lstrip(f'shellies/{self._device_id}/'):
+            self.temperature = message.payload.decode()
+        if 'relay/0' == message.topic.lstrip(f'shellies/{self._device_id}/'):
+            self.state = message.payload.decode()
+        if'relay/0/power' == message.topic.lstrip(f'shellies/{self._device_id}/'):
+            self.power = message.payload.decode()
+        if'plug/data' in message.topic:
+            self.mqtt_client.publish('plug/data/info', self.__str__())
+        if time.time() - self._subscription_tstamps[message.topic] > self._log_interval:
+            self._logger.debug(f'Received message on topic: {message.topic} and data: {message.payload.decode()}')
+            self._subscription_tstamps[message.topic] = time.time()
+
+    def __str__(self):
+        return f'time: {time.asctime()}, state: {self.state}, power: {self.power}, temperature: {self.temperature}'
+
+
+if __name__ == '__main__':
+    import configparser
+    config = configparser.ConfigParser()
+    config.read('heater_config.ini')
+
+    logging.basicConfig(
+        format='%(asctime)s | %(name)-10s | %(levelname)-10s | %(message)s',
+        level=logging.INFO,
+        datefmt='%Y/%d/%m %H:%M:%S')
+
+    mqtt_data = {
+        'mqtt_user': config['MQTT']['mqtt_user'],
+        'mqtt_password': config['MQTT']['mqtt_password'],
+        'mqtt_broker': config['MQTT']['mqtt_broker'],
+        'mqtt_device_id': config['MQTT']['mqtt_device_id'],
+        'mqtt_port': config['MQTT'].getint('mqtt_port')
+    }
+    plug = PlugDevice(**mqtt_data)
+
+
---
+++ solar.py
@@ -0,0 +1,156 @@
+import time
+import datetime
+import logging
+import fusion_solar_py.client as fsc
+import fusion_solar_py.exceptions as fsc_exceptions
+
+solar_logger = logging.getLogger(__name__)
+
+
+class FusionSolarClientExtended(fsc.FusionSolarClient):
+    """
+    Subclass of FusionSolarClient that overrides the get_plant_stats method
+    to allow querying aggregate data for day, month, year or complete lifetime
+    """
+    @fsc.logged_in
+    def get_plant_stats(self, plant_id: str,
+                        query_time: int = round(datetime.datetime.utcnow().timestamp()) * 1000,
+                        stat_type: str = 'day') -> dict:
+        """
+        Queries data for day, month, year or complete lifetime of the plant
+        :param plant_id:
+        :param query_time: ``unix timestamp * 1000`` of the day. If stat_type = 'day' it can be any timestamp in the day.
+            If stat_type = 'month', it can be any timestamp of the month and so on
+        :param stat_type: str with the aggregate type for data:
+        '``day``' for daily data,
+        '``month``' for monthly data,
+        '``year``' for yearly data,
+        '``lifetime``' for lifetime data,
+
+        :return:
+        """
+        date = datetime.datetime.fromtimestamp(query_time / 1000)
+        if stat_type.lower() == 'day':
+            stat_dim = 2   # Day
+            date = date - datetime.timedelta(hours=date.hour, minutes=date.minute,
+                                             seconds=date.second, microseconds=date.microsecond)
+        elif stat_type.lower() == 'month':
+            stat_dim = 4   # Month
+            date = date - datetime.timedelta(days=date.day - 1, hours=date.hour, minutes=date.minute,
+                                             seconds=date.second, microseconds=date.microsecond)
+        elif stat_type.lower() == 'year':
+            stat_dim = 5   # Year
+            date.replace(month=1)
+            date = date - datetime.timedelta(days=date.day - 1, hours=date.hour, minutes=date.minute,
+                                             seconds=date.second, microseconds=date.microsecond)
+        elif stat_type.lower() == 'lifetime':
+            stat_dim = 6   # Lifetime
+        else:
+            stat_dim = 2
+            date = date - datetime.timedelta(hours=date.hour, minutes=date.minute,
+                                             seconds=date.second, microseconds=date.microsecond)
+
+        url = f'https://{self._huawei_subdomain}.fusionsolar.huawei.com/rest/pvms/web/station/v1/overview/energy-balance'
+        params = {
+            'stationDn': plant_id,
+            'timeDim': stat_dim,
+            'queryTime': round(date.timestamp()) * 1000,
+            'timeZone': 2,
+            'timeZoneStr': 'Europe/Viena',
+            'dateStr': date.strftime('%Y-%m-%d %H:%M:%S'),
+            "_": round(time.time() * 1000)
+        }
+        r = self._session.get(url=url,
+                              params=params)
+        r.raise_for_status()
+        plant_data = r.json()
+
+        if not plant_data["success"] or "data" not in plant_data:
+            raise fsc_exceptions.FusionSolarException(
+                f"Failed to retrieve plant status for {plant_id}"
+            )
+
+        # return the plant data
+        return plant_data["data"]
+
+
+class PowerDevice:
+    """
+    Class representing data from a FusionSolar station
+    """
+    def __init__(self, user: str, password: str):
+        """
+        Creates a power / energy data device
+        :param user:
+        :param password:
+        :raise AuthenticationException if credentials are incorrect
+        """
+        try:
+            self.client = FusionSolarClientExtended(user, password)
+        except fsc_exceptions.AuthenticationException as except1:
+            solar_logger.error(f'Logging error with user: {user} and password: {password}. {except1.args}')
+            raise except1
+        self.plant_ids = self.client.get_plant_ids()
+        self._plant_id = self.plant_ids[0]
+
+    def get_inst_pwr(self, tstamp: time.struct_time = time.localtime()) -> dict:
+        """
+        Returns a dictionary with requested timestamp string, produced power and used power.
+        If no timestamp is provided, data for the current day is returned.
+        :param tstamp: time tuple of the requested timestamp
+        :return:
+        """
+        plant_data = self.get_overview(datetime.datetime.fromtimestamp(time.mktime(tstamp)))
+        data_idx = tstamp.tm_hour * 60 // 5 + tstamp.tm_min // 5
+        if plant_data['productPower'][data_idx] != '--':
+            product_pwr = float(plant_data['productPower'][data_idx])
+        else:
+            product_pwr = 0.0
+
+        if plant_data['usePower'][data_idx] != '--':
+            use_power = float(plant_data['usePower'][data_idx])
+        else:
+            use_power = 0.0
+        return {'timestamp': plant_data['xAxis'][data_idx],
+                'productPower': product_pwr,
+                'usePower': use_power}
+
+    def get_overview(self,
+                     date: datetime.datetime = datetime.datetime.now(),
+                     stat_type: str = 'day') -> dict:
+        """
+        Returns all the information for a specific aggregate type
+        :param date: any datetime inside the desired time range
+        :param stat_type: str with the aggregate type for data:
+        '``day``' for daily data,
+        '``month``' for monthly data,
+        '``year``' for yearly data,
+        '``lifetime``' for lifetime data,
+        :return:
+        """
+
+        return self.client.get_plant_stats(self._plant_id,
+                                           query_time=round(date.timestamp()) * 1000, stat_type=stat_type)
+
+
+if __name__ == '__main__':
+    def get_tstamp(year: int, month: int, day: int, hour: int, minute: int) -> time.struct_time:
+        return time.strptime(f'{year}{month}{day}{hour}{minute}00', '%Y%m%d%H%M%S')
+
+    import configparser
+    config = configparser.ConfigParser()
+    config.read('heater_config.ini')
+    huawei_user = config['HUAWEI']['huawei_user']
+    huawei_password = config['HUAWEI']['huawei_password']
+
+    my_data = PowerDevice(huawei_user, huawei_password)
+    pwr_stats = my_data.get_inst_pwr()
+    print(f'Date: {pwr_stats["timestamp"]}')
+    print(f'Current produced power: {pwr_stats["productPower"]} kW')
+    print(f'Current consumed power: {pwr_stats["usePower"]} kW')
+
+    request_date = datetime.datetime(2023, 2, 24, 8, 30)
+    pwr_hist_stats = my_data.get_inst_pwr(request_date.timetuple())
+    print(f'Date: {request_date.isoformat(sep=" ")}')
+    print(f'Produced power : {pwr_hist_stats["productPower"]} kW')
+    print(f'Consumed power: {pwr_hist_stats["usePower"]} kW')
